410961acef060c83e09379c5acb2131313c009ca
Bill Gribble <grib@billgribble.com>
2010-07-31 07:04:38 -0400

    Improve key interpretation to account for left and right modifiers

--- mfp/builtins/p_metro.py
+++ mfp/builtins/p_metro.py
@@ -8,7 +8,7 @@ Copyright (c) 2010 Bill Gribble <grib@billgribble.com>
 from ..timer import MultiTimer 
 from ..processor import Processor
 from ..main import MFPApp 
-from ..datetime import datetime, timedelta 
+from datetime import datetime, timedelta 
 from .. import Bang
 
 class MetroTick (object):
--- mfp/gtk/key_sequencer.py
+++ mfp/gtk/key_sequencer.py
@@ -5,11 +5,17 @@ key_sequencer.py: Collect modifiers and key/mouse clicks into Emacs-like strings
 Copyright (c) 2010 Bill Gribble <grib@billgribble.com>
 '''
 
-MOD_SHIFT = 50
-MOD_RSHIFT = 62
-MOD_CTRL = 66
-MOD_ALT = 64
-MOD_WIN = 133
+MOD_SHIFT = 65505
+MOD_RSHIFT = 65506
+MOD_CTRL = 65507
+MOD_RCTRL = 65508
+MOD_ALT = 65513
+MOD_RALT = 65514
+MOD_WIN = 65515
+MOD_RWIN = 65516
+MOD_ALL = [MOD_SHIFT, MOD_RSHIFT, MOD_CTRL, MOD_RCTRL, MOD_ALT, 
+		   MOD_RALT, MOD_WIN, MOD_RWIN]
+
 KEY_ESC = 65307 
 KEY_TAB = 65289 
 KEY_BKSP = 65288
@@ -45,24 +51,20 @@ class KeySequencer (object):
 	
 		# KEY PRESS 
 		if event.type == clutter.KEY_PRESS: 
-			code = event.get_key_code()
-			if code in (MOD_SHIFT, MOD_CTRL, MOD_ALT, MOD_WIN):
+			code = event.get_key_symbol()
+			if code in MOD_ALL:
 				self.mod_keys.add(code)
-			elif code == MOD_RSHIFT:
-				self.mod_keys.add(MOD_SHIFT)
 			else:
 				self.sequences.append(self.canonicalize(event))
 
 		# KEY RELEASE 
 		elif event.type == clutter.KEY_RELEASE:
-			code = event.get_key_code()
-			if code in (MOD_SHIFT, MOD_CTRL, MOD_ALT, MOD_WIN):
+			code = event.get_key_symbol()
+			if code in MOD_ALL:
 				try:
 					self.mod_keys.remove(code)
 				except KeyError:
 					pass
-			elif code == MOD_RSHIFT:
-				self.mod_keys.remove(MOD_SHIFT)
 
 		# BUTTON PRESS, BUTTON RELEASE, MOUSE MOTION
 		elif event.type in (clutter.BUTTON_PRESS, clutter.BUTTON_RELEASE, clutter.MOTION):
@@ -72,16 +74,16 @@ class KeySequencer (object):
 		import clutter 
 		key = ''
 		
-		if MOD_CTRL in self.mod_keys:
+		if (MOD_CTRL in self.mod_keys) or (MOD_RCTRL in self.mod_keys):
 			key += 'C-'
-		if MOD_ALT in self.mod_keys: 
+		if (MOD_ALT in self.mod_keys) or (MOD_RALT in self.mod_keys): 
 			key += 'A-'
-		if MOD_WIN in self.mod_keys:
+		if (MOD_WIN in self.mod_keys) or (MOD_RWIN in self.mod_keys):
 			key += 'W-'
 
 		if event.type in (clutter.KEY_PRESS, clutter.KEY_RELEASE):
 			ks = event.get_key_symbol()
-			if ks >= 256 and MOD_SHIFT in self.mod_keys:
+			if ks >= 256 and (MOD_SHIFT in self.mod_keys) or (MOD_RSHIFT in self.mod_keys):
 				key = 'S-' + key 
 		    	
 			if ks == KEY_TAB:
@@ -102,11 +104,13 @@ class KeySequencer (object):
 				key += 'DEL'
 			elif ks == KEY_BKSP:
 				key += 'BS'
+			elif ks == KEY_INS:
+				key += 'INS'
 			elif ks < 256:
 				kuni = event.get_key_unicode()
 				if kuni < 32:
 					ks = chr(event.get_key_symbol())
-					if MOD_SHIFT in self.mod_keys:
+					if (MOD_SHIFT in self.mod_keys) or (MOD_RSHIFT in self.mod_keys):
 						ks = ks.upper()
 					key += ks 
 				else:
@@ -136,6 +140,7 @@ class KeySequencer (object):
 					key += 'M%d-' % b
 			key += 'MOTION'
 		
+		print key
 		return key 	
 
 
