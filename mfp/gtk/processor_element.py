041a3bf50b3a6cbf0270aa6f0d9d934bc370105b
Bill Gribble <grib@billgribble.com>
2011-10-03 21:46:43 -0400

    BROKEN: begin refactor of display elements

--- mfp/gtk/processor_element.py
+++ mfp/gtk/processor_element.py
@@ -14,40 +14,85 @@ from .modes.label_edit import LabelEditMode
 class ProcessorElement (PatchElement):
 	element_type = "processor"
 
+	# constants 
+	port_border = 5
+	port_minspace = 10
+
 	def __init__(self, window, x, y, params={}):
 		PatchElement.__init__(self, window, x, y)
-	
-		self.connections_out = [] 
-		self.connections_in = [] 
+		
+		# display elements 
+		self.actor = None
+		self.rect = None
+		self.label = None
+		self.ports_in = [] 
+		self.ports_out = []
 
-		# create elements 
+		# create display 
+		self.create()
+		self.set_size(35, 20)
+		self.move(x, y)
+		self.update()
+
+		# add components to stage 
+		self.stage.register(self)
+
+	def create(self):
 		self.actor = clutter.Group()
 		self.rect = clutter.Rectangle()
 		self.label = clutter.Text()
 
-		# configure rectangle box 
-		self.rect.set_size(35, 20)
+		# rectangle box 
 		self.rect.set_border_width(2)
 		self.rect.set_border_color(window.color_unselected)
 		self.rect.set_reactive(False)
+		# FIXME not-created style (dashed lines?)
 
-		# configure label
-		self.label.set_position(4, 1)
+		# label
+		self.label.set_position(4, 3)
 		self.label.set_color(window.color_unselected) 
-		self.label.connect('text-changed', self.text_changed_cb)
+		self.label.connect('text-changed', self.label_changed_cb)
 		self.label.set_reactive(False)
 
 		self.actor.add(self.rect)
 		self.actor.add(self.label)
 		self.actor.set_reactive(True)
 
-		self.move(x, y)
+	def update(self):
+		# FIXME not-created style (dashed lines?)
 
-		# add components to stage 
-		self.stage.register(self)
+		self.update_ports()
 
-	def update_label(self, *args):
-		print "processor update label", args
+	def get_label(self):
+		return self.label
+
+	def draw_ports(self): 
+		for i in 
+
+	def port_position(self, port_dir, port_num):
+		w = self.rect.get_width()
+		h = self.rect.get_height()
+
+		if port_dir == PatchElement.PORT_IN:
+			if self.num_inlets < 2:
+				spc = 0
+			else:
+				spc = min(self.port_minspace, 
+						  (w-2.0*self.port_border) / (self.num_inlets-1))
+			return (self.port_border + spc*port_num, h)
+
+		elif port_dir == PatchElement.PORT_OUT:
+			if self.num_outlets < 2:
+				spc = 0
+			else:
+				spc = min(self.port_minspace, 
+						  (w-2.0*self.port_border) / (self.num_outlets-1))
+			return (self.port_border + spc*port_num, h)
+
+	def label_edit_start(self):
+		pass
+
+	def label_edit_finish(self, *args):
 		t = self.label.get_text()
 		parts = t.split(' ', 1)
 		self.obj_type = parts[0]
@@ -59,9 +104,11 @@ class ProcessorElement (PatchElement):
 		self.obj_id = MFPGUI().mfp.create(self.obj_type, self.obj_args)
 		if self.obj_id is None:
 			print "ProcessorElement: could not create", self.obj_type, self.obj_args
-		self.send_params()
+		else:
+			self.send_params()
+		self.update()
 
-	def text_changed_cb(self, *args):
+	def label_changed_cb(self, *args):
 		'''called by clutter when label.set_text or editing occurs'''
 
 		lwidth = self.label.get_property('width') 
@@ -74,12 +121,26 @@ class ProcessorElement (PatchElement):
 			new_w = max(35, lwidth + 14)
 
 		if new_w is not None:
-			self.rect.set_size(new_w, self.rect.get_property('height'))
+			self.set_size(new_w, self.rect.get_property('height'))
 
 	def move(self, x, y):
 		self.position_x = x
 		self.position_y = y
 		self.actor.set_position(x, y)
+
+		for c in self.connections_out:
+			c.draw()
+		
+		for c in self.connections_in:
+			c.draw()
+
+	def set_size(self, w, h):
+		self.size_w = w
+		self.size_h = h 
+
+		self.rect.set_size(w, h)
+
+		self.draw_ports()
 
 		for c in self.connections_out:
 			c.draw()
@@ -96,7 +157,6 @@ class ProcessorElement (PatchElement):
 		self.rect.set_border_color(self.stage.color_unselected)
 
 	def delete(self):
-		print "processor delete:", self
 		for c in self.connections_out+self.connections_in:
 			c.delete()
 
