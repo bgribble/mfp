f06d72e27341538888fd7ec73f661b278b1b53a2
Bill Gribble <grib@billgribble.com>
2011-12-11 15:35:27 -0500

    Add axes and start legend

--- mfp/gtk/scopetest.py
+++ mfp/gtk/scopetest.py
@@ -3,24 +3,22 @@ import clutter
 black = clutter.color_from_string("Black")
 white = clutter.color_from_string("White")
 
-def subdivide(vmin, vmax, scale):
-	'''
-	norm = log10 of range - 1
-	approx ticksize = range/approx tick count 
-	closest of (1, .5, .25, .1) to normtick
-	all multiples between vmin, vmax
-	'''
-	pts = [ vmin, vmax ]
-	numpts = int(scale / self.MIN_DIV_SIZE)
-	interv = (vmax-vmin)/numpts
-
-	pts.sort()
-	return pts
+def mkticks(vmin, vmax, numticks):
+	interv = float(vmax-vmin)/numticks
+	logbase = pow(10, math.floor(math.log10(interv)))
+	choices = [ logbase, logbase*2.5, logbase*5.0, logbase * 10]
+	diffs = [ abs(interv-c) for c in choices ]
+	tickint = choices[diffs.index(min(*diffs))]
+	tickbase = tickint * math.floor(vmin / tickint)
+	numticks = int(float(vmax-vmin) / tickint) + 2
+	ticks = [ tickbase + n*tickint for n in range(numticks) ]
+	return [ t for t in ticks if t >= vmin and t <= vmax ]
 
 class ScopeActor(object):
 	MARGIN_LEFT = 25
 	MARGIN_BOT = 25
-	MIN_DIV_SIZE = 50
+	AXIS_PAD = 5
+	TICK_SIZE = 50
 
 	def __init__(self, stage, width, height, title):
 		self.stage = stage
@@ -61,14 +59,14 @@ class ScopeActor(object):
 		self.cl_title.set_position(0, 0)
 		self.cl_group.add(self.cl_title)
 
-		self.cl_field_w = self.width - 2*self.MARGIN_LEFT
-		self.cl_field_h = self.height - 2*self.MARGIN_BOT
+		self.cl_field_w = self.width - 3*self.MARGIN_LEFT
+		self.cl_field_h = self.height - 3*self.MARGIN_BOT
 
-		self.cl_xaxis_bg = clutter.CairoTexture(self.cl_field_w, self.MARGIN_BOT)
-		self.cl_xaxis_bg.set_position(self.MARGIN_LEFT, self.height-self.MARGIN_BOT)
+		self.cl_xaxis_bg = clutter.CairoTexture(self.cl_field_w, 2*self.MARGIN_BOT)
+		self.cl_xaxis_bg.set_position(2*self.MARGIN_LEFT, self.height-2*self.MARGIN_BOT)
 		self.cl_group.add(self.cl_xaxis_bg)
 
-		self.cl_yaxis_bg = clutter.CairoTexture(self.MARGIN_LEFT, self.cl_field_h)
+		self.cl_yaxis_bg = clutter.CairoTexture(2*self.MARGIN_LEFT, self.cl_field_h)
 		self.cl_yaxis_bg.set_position(0, self.MARGIN_BOT)
 		self.cl_group.add(self.cl_yaxis_bg)
 
@@ -77,37 +75,77 @@ class ScopeActor(object):
 		self.cl_field.set_border_color(black)
 		self.cl_field.set_color(white)
 		self.cl_field.set_size(self.cl_field_w, self.cl_field_h)
-		self.cl_field.set_position(self.MARGIN_LEFT, self.MARGIN_BOT)
+		self.cl_field.set_position(2*self.MARGIN_LEFT, self.MARGIN_BOT)
 		self.cl_group.add(self.cl_field)
 
 		self.cl_curve = clutter.CairoTexture(self.cl_field_w, self.cl_field_h)
-		self.cl_curve.set_position(self.MARGIN_LEFT, self.MARGIN_BOT)
+		self.cl_curve.set_position(2*self.MARGIN_LEFT, self.MARGIN_BOT)
 		self.cl_group.add(self.cl_curve)
 
 		self.stage.add(self.cl_group)
 
+	def pt_pos(self, p):
+		np = [(p[0] - self.x_min)*float(self.cl_field_w)/(self.x_max - self.x_min),
+		      self.cl_field_h - (p[1] - self.y_min)*float(self.cl_field_h)/(self.y_max - self.y_min)]
+		return np
+
 	def draw_axes(self):
+		# X axis
+		ticks = mkticks(self.x_min, self.x_max, self.cl_field_w/self.TICK_SIZE)
+		self.cl_xaxis_bg.clear()
+		ctx = self.cl_xaxis_bg.cairo_create()
+		ctx.set_source_color(black)
+		ctx.set_font_size(8)
+
+		# the axis line
+		ctx.move_to(0, self.AXIS_PAD)
+		ctx.line_to(self.cl_field_w, self.AXIS_PAD)
+		ctx.stroke()
+
+		# ticks
+		for tick in ticks:
+			p = self.pt_pos([tick, 0])
+			ctx.move_to(p[0], self.AXIS_PAD)
+			ctx.line_to(p[0], 3*self.AXIS_PAD)
+			ctx.stroke()
+			ctx.move_to(p[0], self.AXIS_PAD+self.MARGIN_BOT)
+			ctx.show_text("%.3g" % tick)
+		del ctx
+
+		# Y axis
+		ticks = mkticks(self.y_min, self.y_max, float(self.cl_field_h)/self.TICK_SIZE)
+		self.cl_yaxis_bg.clear()
+		ctx = self.cl_yaxis_bg.cairo_create()
+		ctx.set_source_color(black)
+		
+		# the axis line
+		ctx.move_to(2*self.MARGIN_LEFT-self.AXIS_PAD, 0)
+		ctx.line_to(2*self.MARGIN_LEFT-self.AXIS_PAD, self.cl_field_h)
+		ctx.stroke()
+
+		# ticks
+		for tick in ticks:
+			p = self.pt_pos([0, tick])
+			ctx.move_to(2*self.MARGIN_LEFT-self.AXIS_PAD, p[1])
+			ctx.line_to(2*self.MARGIN_LEFT-3*self.AXIS_PAD, p[1])
+			ctx.stroke()
+
+		
+
+		del ctx
 
 	def draw_curve(self, points):
-		def cvt(p):
-			np = [(p[0] - self.x_min)*float(self.cl_field_w)/(self.x_max - self.x_min),
-		          self.cl_field_h - (p[1] - self.y_min)*float(self.cl_field_h)/(self.y_max -
-																		  self.y_min)]
-			return np
-
 		self.cl_curve.clear()
 		ctxt = self.cl_curve.cairo_create()
 		ctxt.scale(1.0, 1.0)
 		ctxt.set_source_color(black)
 
-		p = cvt(points[0])
+		p = self.pt_pos(points[0])
 		ctxt.move_to(p[0], p[1])
 
 		for p in points[1:]:
-			pc = cvt(p)
-			#print pc
+			pc = self.pt_pos(p)
 			ctxt.line_to(pc[0], pc[1])
-		#ctxt.close_path()
 		ctxt.stroke()
 		del ctxt
 
@@ -115,7 +153,7 @@ class ScopeActor(object):
 import math
 import glib
 
-pts = [[x/100.0, math.sin(x/100.0)] for x in range(0, 628) ]
+pts = [[x/100.0, math.sin(x/100.0)] for x in range(0, 6280) ]
 
 if __name__ == "__main__":
 	
@@ -129,15 +167,9 @@ if __name__ == "__main__":
 	stg.show()
 	
 	def movecurve():
-		global pts
-		fx = pts[-0][0]
-		for p in pts[1:]:
-			nx = p[0]]
-			p[0] = fx
-			fx = nx
-			
-		pts[0][0] = fx
-		pts.sort()
+		sco.x_min += 0.1
+		sco.x_max += 0.1
+		sco.draw_axes()
 		sco.draw_curve(pts)
 		return True
 
