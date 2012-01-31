ec27f86f9c02031da51154e610a04d2ce9cc0b67
Bill Gribble <grib@billgribble.com>
2012-01-31 05:51:03 -0500

    Add midi_in, midi_out objects

---
+++ mfp/builtins/p_midi.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+'''
+p_midi.py: builtins for MIDI I/O
+
+Copyright (c) 2012 Bill Gribble <grib@billgribble.com>
+'''
+
+from .. import midi
+
+class MidiIn (Processor):
+	def __init__(self, init_type, init_args):
+		Processor.__init__(self, 1, 1, init_type, init_args)
+
+		self.port = 0
+		self.channels = []
+
+		midi.register(self.send)
+
+	def trigger(self):
+		event = self.inlets[0]
+		if isinstance(event, dict):
+			for attr, val in event.items():
+				setattr(self, attr, val)
+		elif isinstance(event, MethodCall):
+			self.method(event, 0)
+		elif isinstance(event, [ NoteEvent, MidiCCEvent, MidiMiscEvent ]):
+			if event.port == self.port and (self.channels == [] or event.channel in self.channels):
+				self.outlets[0] = event
+
+class MidiOut (Processor):
+	def __init__(self, init_type, init_args):
+		self.port = 0
+
+	def trigger(self):
+		event = self.inlets[0]
+
+		if isinstance(event, dict):
+			for attr, val in event.items():
+				setattr(self, attr, val)
+		elif isinstance(event, MethodCall):
+			self.method(event, 0)
+		elif isinstance(event, [ NoteEvent, MidiCCEvent, MidiMiscEvent ]):
+			event.port = self.port
+			midi.send(event)
+
