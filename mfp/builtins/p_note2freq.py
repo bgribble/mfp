b21768cb53a08beac72edb6bb906896318b3c3ef
Bill Gribble <grib@billgribble.com>
2012-03-05 09:09:43 -0500

    Add note to frequency, tuning, scales

---
+++ mfp/builtins/p_note2freq.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+'''
+p_note2freq.py:  Convert a NoteOn or note number to freq in Hz 
+
+Copyright (c) 2012 Bill Gribble <grib@billgribble.com>
+'''
+
+from ..processor import Processor
+from ..main import MFPApp
+from .. import Bang, Uninit 
+
+class Note2Freq(Processor):
+	'''
+	[note2freq] converts a note to a frequency
+	Parameters:
+		scale: a subclass of Scale (defaults to Chromatic)
+		tuning: an instance of Tuning (defaults to EqualTemper)
+	'''
+	def __init__(self, init_type, init_args):
+		(pos, kwd) = self.parse_args(init_args)
+		if kwd.get('scale'):
+			self.scale = kwd.get('scale')
+		else:
+			self.scale = scale.Chromatic()
+
+		if kwd.get('tuning'):
+			self.tuning = kwd.get('tuning')
+		else:
+			self.tuning = scale.EqualTemper()
+
+	def trigger(self):
+		inval = self.inlets[0]
+		note = None
+		if isinstance(inval, midi.Note):
+			note = inval.key
+		elif isinstance(inval, (float, int)):
+			note = int(inval)
+		elif isinstance(inval, Scale):
+			self.scale = inval
+		elif isinstance(inval, Tuning):
+			self.tuning = inval
+		elif isinstance(inval, dict):
+			if inval.has_key('scale'):
+				self.scale = inval.get('scale')
+			if inval.has_key('tuning'):
+				self.tuning = inval.get('tuning')
+
+		if note is not None:
+			self.outlets[0] = self.tuning.freq(self.scale.midinote(note))
+
---
+++ mfp/scale.py
@@ -0,0 +1,42 @@
+
+class Scale (object):
+	def __init__(self):
+		pass
+
+class Tuning (object):
+	def __init__(self):
+		pass
+
+class EqualTemperament (Tuning):
+	def __init__(self, a4=440.0):
+		Tuning.__init__(self)
+		self.base_frequencies = [ 261.63, 277.18, 293.66, 311.13, 329.63,
+				   		          349.23, 369.99, 392.00, 415.30, 440.00,
+						          466.16, 493.88 ] 
+		self.a4_freq = a4
+		self.frequencies = []
+		self.retune(self.a4_freq)
+
+	def retune(self, a4):
+		scaling = a4 / 440.0
+		self.a4_freq = a4
+		self.frequencies = [ f*scaling for f in self.base_frequencies ]
+
+	def freq(self, octave, tone):
+		octave_scale = 2.0**(octave - 4)
+		return self.frequencies[tone] * octave_scale
+
+class Chromatic (Scale):
+	def __init__(self, transpose=0):
+		Scale.__init__(self)
+		self.transpose_semis = transpose
+
+	def transpose(self, offset):
+		self.transpose_semis = offset
+
+	def midinote(self, keynum):
+		note = keynum + self.transpose_semis
+		octave = int(note) / 12 - 2
+		tone = int(note) % 12 
+		return (octave, tone)
+
