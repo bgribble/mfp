6863e306c85f200f754cf097941eb9fb206ac2fe
Bill Gribble <grib@billgribble.com>
2012-10-05 16:25:37 -0400

    Add methods for roll mode to chart object

--- mfp/builtins/p_chart.py
+++ mfp/builtins/p_chart.py
@@ -26,46 +26,63 @@ class Chart (Processor):
 			message.kwargs['inlet'] = inlet
 		message.call(self)
 
+	def _time(self):
+		return (datetime.now() - self.time_base).total_seconds()
+
+	def _chartconf(self, action, data=None):
+		self.gui_params['_chart_action'] = action
+		self.gui_params['_chart_data'] = data
+
+		MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
+		del self.gui_params['_chart_action']
+		del self.gui_params['_chart_data']
+	
 	def trigger(self):
 		points = {}
 		for i, val in zip(range(len(self.inlets)), self.inlets):
+			v = None
 			if isinstance(val, (tuple, list)):
 				v = tuple(val)
+			elif isinstance(val, (float, int)):
+				v = (self._time(), val)
+
+			if v is not None:
 				cpts = self.points.setdefault(i, [])
 				cpts.append(v)
 				cpts = points.setdefault(i, [])
 				cpts.append(v)
-			else:
-				pass
 			self.inlets[i] = Uninit
 
 		if points != {}:
-			self.gui_params['_chart_action'] = 'add'
-			self.gui_params['_chart_data'] = points
+			self._chartconf('add', points)
 
-			MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
-			self.finish_action()
+	# methods that the object responds to 
+	def roll(self, **kwargs):
+		'''Start the chart roll function.'''
+		self._chartconf('roll', self._time())
+
+	def stop(self, **kwargs):
+		'''Stop the chart roll'''
+		self._chartconf('stop', self._time())
+
+	def reset(self, **kwargs):
+		'''Reset time base for items with no X'''
+		self.time_base = datetime.now()
+		self._chartconf('reset', self.time_base)
 
 	def clearall(self, **kwargs):
+		'''Clear all data points'''
 		self.points = {}
-
-		self.gui_params['_chart_action'] = 'clear'
-		self.gui_params['_chart_data'] = None
-
-		MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
-		self.finish_action()
+		self._chartconf('clear')
 
 	def clear(self, inlet=0):
+		'''Clear a single curve's points'''
 		if inlet is not None and self.points.has_key(inlet):
 			del self.points[inlet]
-
-		self.gui_params['_chart_action'] = 'clear'
-		self.gui_params['_chart_data'] = inlet
-
-		MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
-		self.finish_action()
+		self._chartconf('clear', inlet)
 
 	def style(self, **kwargs):
+		'''Set style parameters for a curve'''
 		inlet = kwargs.get('inlet', 0)
 		style = self.gui_params.setdefault('style', {})
 		instyle = style.setdefault(inlet, {})
@@ -76,14 +93,9 @@ class Chart (Processor):
 		MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
 
 	def bounds(self, x_min, y_min, x_max, y_max):
-		self.gui_params['_chart_action'] = 'bounds'
-		self.gui_params['_chart_data'] = (x_min, y_min, x_max, y_max)
-		MFPApp().gui_cmd.configure(self.obj_id, self.gui_params)
-		self.finish_action()
+		'''Set viewport boundaries in chart coordinates'''
+		self._chartconf('bounds', (x_min, y_min, x_max, y_max))
 
-	def finish_action(self):
-		del self.gui_params['_chart_action']
-		del self.gui_params['_chart_data']
 
 
 
