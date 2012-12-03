15fd65962ff803f29f8f346beefd4efc7a2dbb3f
Bill Gribble <grib@billgribble.com>
2012-12-03 13:15:29 -0500

    Add some features for passing in file objects

--- mfp/builtins/file.py
+++ mfp/builtins/file.py
@@ -8,15 +8,22 @@ class FileIO(Processor):
 		self.filename = None 
 		self.fileobj = None 
 		self.mode = "r"
+		self.need_close = True 
 
 		Processor.__init__(self, 1, 1, init_type, init_args, patch, scope, name)
 		initargs, kwargs = self.parse_args(init_args)
-		if len(initargs) > 0:
-			self.filename = initargs[0]
+
 		if len(initargs) > 1:
 			self.mode = initargs[1]
-		if self.filename:
-			self.open()
+		if len(initargs) > 0:
+			if isinstance(initargs[0], file):
+				self.fileobj = initargs[0]
+				self.filename = self.fileobj.name
+				self.mode = self.fileobj.mode
+				self.need_close = False 
+			else: 
+				self.filename = initargs[0]
+				self.open()
 	
 	def trigger(self):
 		cmd = self.inlets[0]
@@ -37,15 +44,21 @@ class FileIO(Processor):
 	def readlines(self):
 		return self.fileobj.readlines()
 
+	def use(self, fileobj):
+		self.fileobj = fileobj
+		self.filename = fileobj.name
+		self.mode = fileobj.mode
+
 	def open(self, filename=None, mode=None):
 		if filename is not None:
 			self.filename = filename
 		if mode is not None:
 			self.mode = mode
 		if self.filename is not None:
-			if self.fileobj:
+			if self.fileobj and self.need_close:
 				self.close()
 			self.fileobj = open(self.filename, self.mode)
+			self.need_close = True 
 
 	def close(self):
 		if self.fileobj:
