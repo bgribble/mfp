8a7f89f60d21447df4ae2f11a263e8766bbdbac6
Bill Gribble <grib@billgribble.com>
2013-02-28 16:20:02 -0500

    GitHub #42: Back out overzealous line delete

--- mfpdsp/mfp_pyglue.c
+++ mfpdsp/mfp_pyglue.c
@@ -442,6 +442,7 @@ init_globals(void)
     mfp_proc_registry = g_hash_table_new(g_str_hash, g_str_equal);
     mfp_proc_objects = g_hash_table_new(NULL, NULL);
     mfp_request_cleanup = g_array_new(TRUE, TRUE, sizeof(mfp_reqdata));
+    mfp_responses_pending = g_array_new(TRUE, TRUE, sizeof(mfp_respdata));
 
     pthread_cond_init(&mfp_response_cond, NULL);
     pthread_mutex_init(&mfp_response_lock, NULL);
