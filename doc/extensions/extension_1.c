d187416a8b49740e7d48e1036a8b9c07afc6ff00
Bill Gribble <grib@billgribble.com>
2013-03-07 13:07:00 -0500

    GitHub #46: Add sample extension to doc/

---
+++ doc/extensions/extension_1.c
@@ -0,0 +1,41 @@
+#include <glib.h>
+#include <stdio.h>
+#include "mfp_dsp.h"
+
+static void 
+init(mfp_processor * proc) 
+{
+    proc->data = (void *)g_malloc(sizeof(int));
+    printf("ext1~ init\n");
+}
+
+static void
+config(mfp_processor * proc) 
+{
+    int * d = (int *)(proc->data);
+    printf("ext1~ config\n");
+    *d = 1;
+
+}
+
+
+static void
+process(mfp_processor * proc) 
+{
+    int * d = (int *)(proc->data);
+    printf("ext1~ process\n");
+    *d = 2;
+}
+
+void
+ext_initialize(void) 
+{
+    mfp_procinfo * p = g_malloc0(sizeof(mfp_procinfo));
+    p->name = g_strdup("ext1~");
+    p->is_generator = GENERATOR_ALWAYS;
+    p->init = init;
+    p->process = process; 
+    p->config = config;
+    p->params = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
+    g_hash_table_insert(mfp_proc_registry, p->name, p);
+}
