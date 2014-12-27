6d5807919cb698aaec028863049ff9a4007a5f22
Bill Gribble <grib@billgribble.com>
2014-12-27 17:05:37 -0500

    GitHub #231: Don't make connections twice when cloning

--- mfp/patch_clonescope.py
+++ mfp/patch_clonescope.py
@@ -99,12 +99,6 @@ def clonescope(self, scopename, num_copies, **kwargs):
                 newobj.gui_params["position_x"] += dx
                 newobj.gui_params["position_y"] += dy
 
-                # FIXME: template expansion in clonescope
-                if newobj.gui_params.get("display_type") == "text":
-                    txtprms = dict(num=copynum+1, row=grid_row, col=grid_col)
-                    newobj.value = newobj.value % txtprms
-                    newobj.gui_params["value"] = newobj.value
-
             if srcobj.gui_created: 
                 newobj.create_gui()
 
@@ -115,18 +109,20 @@ def clonescope(self, scopename, num_copies, **kwargs):
             for port_num, port_conn in enumerate(srcobj.connections_in):
                 for tobj, tport in port_conn:
                     tobj_newid = obj_idmap.get(tobj.obj_id, tobj)
-                    tobj_newid.connect(tport, newobj, port_num)
-                    if tobj.gui_created:
-                        MFPApp().gui_command.connect(tobj_newid.obj_id, tport, 
-                                                     newobj.obj_id, port_num)
+                    if (newobj, port_num) not in tobj_newid.connections_out[tport]:
+                        tobj_newid.connect(tport, newobj, port_num)
+                        if tobj.gui_created:
+                            MFPApp().gui_command.connect(tobj_newid.obj_id, tport, 
+                                                         newobj.obj_id, port_num)
 
             for port_num, port_conn in enumerate(srcobj.connections_out):
                 for tobj, tport in port_conn:
                     tobj_newid = obj_idmap.get(tobj.obj_id, tobj)
-                    newobj.connect(port_num, tobj_newid, tport)
-                    if tobj.gui_created:
-                        MFPApp().gui_command.connect(newobj.obj_id, port_num,
-                                                     tobj_newid.obj_id, tport)
+                    if (tobj_newid, tport) not in newobj.connections_out[port_num]:
+                        newobj.connect(port_num, tobj_newid, tport)
+                        if tobj.gui_created:
+                            MFPApp().gui_command.connect(newobj.obj_id, port_num,
+                                                         tobj_newid.obj_id, tport)
 
 
         # make sure [loadbang] get triggered 
