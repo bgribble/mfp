f052a916cbb1ea5063031ae3b4c01e014bc66c56
Bill Gribble <grib@billgribble.com>
2016-12-02 07:59:47 -0500

    GitHub #282: Don't connect to nonexistent ports

--- mfp/gui/modes/connection.py
+++ mfp/gui/modes/connection.py
@@ -6,7 +6,7 @@ Copyright (c) 2010 Bill Gribble <grib@billgribble.com>
 '''
 from ..input_mode import InputMode
 from ..connection_element import ConnectionElement
-from ..patch_element import PatchElement 
+from ..patch_element import PatchElement
 
 from mfp import MFPGUI
 from mfp import log
@@ -19,7 +19,7 @@ class ConnectionMode (InputMode):
         self.reverse = connect_rev
 
         self.connection = None
-        self.select_cbid = None 
+        self.select_cbid = None
 
         self.source_obj = None
         self.source_port = 0
@@ -51,26 +51,26 @@ class ConnectionMode (InputMode):
         self.select_cbid = self.window.add_callback("select", self.select)
         self.remove_cbid = self.window.add_callback("remove", self.remove_cb)
 
-    def update_connection(self): 
+    def update_connection(self):
         if (self.source_obj is None or self.dest_obj is None):
             if self.connection:
                 self.connection.delete()
-                self.connection = None 
-            return True 
-        
+                self.connection = None
+            return True
+
         if self.connection is None or self.connection.obj_state == PatchElement.OBJ_DELETED:
-            self.connection = ConnectionElement(self.window, 
+            self.connection = ConnectionElement(self.window,
                                                 self.source_obj, self.source_port,
-                                                self.dest_obj, self.dest_port, 
+                                                self.dest_obj, self.dest_port,
                                                 dashed=True)
             self.source_obj.connections_out.append(self.connection)
             self.dest_obj.connections_in.append(self.connection)
-        else: 
+        else:
             if self.connection.obj_1 and self.connection.obj_1 != self.source_obj:
                 if self.connection in self.connection.obj_1.connections_out:
                     self.connection.obj_1.connections_out.remove(self.connection)
                 self.source_obj.connections_out.append(self.connection)
-            
+
             if self.connection.obj_2 and self.connection.obj_2 != self.dest_obj:
                 if self.connection in self.connection.obj_2.connections_in:
                     self.connection.obj_2.connections_in.remove(self.connection)
@@ -79,33 +79,33 @@ class ConnectionMode (InputMode):
             self.connection.obj_1 = self.source_obj
             self.connection.obj_2 = self.dest_obj
             self.connection.port_1 = self.source_port
-            self.connection.port_2 = self.dest_port 
+            self.connection.port_2 = self.dest_port
 
             self.connection.draw()
 
     def select(self, obj):
-        if not obj.editable: 
+        if not obj.editable:
             return
 
         if self.reverse:
             if self.dest_obj is not None and obj and self.dest_obj != obj:
                 self.source_obj = obj
-        else: 
+        else:
             if self.source_obj is not None and obj and self.source_obj != obj:
                 self.dest_obj = obj
 
         self.update_connection()
 
     def remove_cb(self, obj):
-        if obj is self.connection: 
-            self.connection = None 
-        elif obj is self.dest_obj: 
-            self.dest_obj = None 
-            if self.reverse: 
+        if obj is self.connection:
+            self.connection = None
+        elif obj is self.dest_obj:
+            self.dest_obj = None
+            if self.reverse:
                 self.manager.disable_minor_mode(self)
         elif obj is self.source_obj:
             self.source_obj = None
-            if not self.reverse: 
+            if not self.reverse:
                 self.manager.disable_minor_mode(self)
 
 
@@ -117,20 +117,20 @@ class ConnectionMode (InputMode):
 
         if self.connection:
             self.connection.delete()
-            self.connection = None 
+            self.connection = None
 
-    def get_port_key(self): 
-        def callback(txt): 
+    def get_port_key(self):
+        def callback(txt):
             self.set_port_key(int(txt))
 
-        if ((self.dest_obj is not None) 
+        if ((self.dest_obj is not None)
             or (self.window.selected and self.window.selected[0] != self.source_obj)):
             dirspec = "destination input"
-        else: 
+        else:
             dirspec = "source output"
 
         self.window.get_prompted_input("Enter %s port:" % dirspec, callback)
-        return True 
+        return True
 
     def make_connection(self):
         # are both ends selected?
@@ -140,7 +140,7 @@ class ConnectionMode (InputMode):
         if not self.reverse and self.dest_obj is None and self.window.selected:
             self.dest_obj = self.window.selected[0]
 
-        if (self.source_obj and self.dest_obj 
+        if (self.source_obj and self.dest_obj
             and self.connection.obj_state != PatchElement.OBJ_DELETED):
             if MFPGUI().mfp.connect(self.source_obj.obj_id, self.source_port,
                                     self.dest_obj.obj_id, self.dest_port):
@@ -167,17 +167,17 @@ class ConnectionMode (InputMode):
                 self.source_obj = self.window.selected[0]
 
             if self.source_obj is not None:
-                self.source_port = portnum
+                self.source_port = max(0, min(portnum, self.source_obj.num_outlets-1))
             else:
-                self.dest_port = portnum
+                self.dest_port =  max(0, min(portnum, self.dest_obj.num_inlets-1))
         else:
             if (self.dest_obj is None and self.window.selected and
                     self.window.selected[0] != self.source_obj):
                 self.dest_obj = self.window.selected[0]
 
             if self.dest_obj is not None:
-                self.dest_port = portnum
+                self.dest_port = max(0, min(portnum, self.dest_obj.num_inlets-1))
             else:
-                self.source_port = portnum
+                self.source_port = max(0, min(portnum, self.source_obj.num_outlets-1))
         self.update_connection()
         return True
