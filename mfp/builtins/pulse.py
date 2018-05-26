bc48f390ad13ecaf7400f642f5262f0b20e39801
Bill Gribble <grib@billgribble.com>
2018-05-26 08:57:52 -0400

    Add fixed-pulse-width mode to [pulse~]/[gate~]

--- mfp/builtins/pulse.py
+++ mfp/builtins/pulse.py
@@ -8,27 +8,43 @@ Copyright (c) 2018 Bill Gribble <grib@billgribble.com>
 from mfp.processor import Processor
 from ..mfp_app import MFPApp
 from .. import Bang, Uninit
-from mfp import log
 
 
 class Pulse(Processor):
+    '''
+    pulse train oscillator
+    [pulse~] is (-1,1), [gate~] is (0,1)
+
+    To create a fixed-width pulse (for pulse~ or gate~)
+    include creation args with the PW and 'ms' as
+    last args. Default or 'frac' as last arg means
+    pulse width is a fraction of the overall cycle
+
+    example:
+    [pulse~ 100, 1, 10, 'ms']
+    100 Hz, amplitude 1, 10 ms pulse width
+    '''
+
     doc_tooltip_obj = "Pulse/square oscillator"
-    doc_tooltip_inlet = ["Phase reset (radians)", 
-                         "Frequency (hz) (min: 0, max: samplerate/2, default: initarg 0)", 
+    doc_tooltip_inlet = ["Phase reset (radians)",
+                         "Frequency (hz) (min: 0, max: samplerate/2, default: initarg 0)",
                          "Amplitude (default: initarg 1)",
-                         "Pulse Width (default: initarg 2)"]
-    doc_tooltip_outlet = ["Signal output" ]
+                         "Pulse Width (default: initarg 2)",
+                         "Pulse Width Mode ('ms' or 'frac') (default: initarg 3)"]
+    doc_tooltip_outlet = ["Signal output"]
 
     def __init__(self, init_type, init_args, patch, scope, name):
         Processor.__init__(self, 4, 1, init_type, init_args, patch, scope, name)
         initargs, kwargs = self.parse_args(init_args)
         freq = 0
-        ampl = 1.0 
+        ampl = 1.0
         pw = 0.5
-        loval = -1.0 
+        loval = -1.0
         hival = 1.0
+        mode = 0
+
         if init_type == 'gate~':
-            loval = 0.0;
+            loval = 0.0
 
         if len(initargs) > 0:
             freq = initargs[0]
@@ -36,16 +52,23 @@ class Pulse(Processor):
             ampl = initargs[1]
         if len(initargs) > 2:
             pw = initargs[2]
+        if len(initargs) > 3:
+            if initargs[3] == 'ms':
+                mode = 1
+            elif initargs[3] == 'frac':
+                mode = 0
 
         self.dsp_inlets = [1, 2, 3]
         self.dsp_outlets = [0]
-        self.dsp_init("pulse~", 
-                      _sig_1=float(freq), 
-                      _sig_2=float(ampl),
-                      _sig_3=float(pw),
-                      hival=hival,
-                      loval=loval
-                     )
+        self.dsp_init(
+            "pulse~",
+            _sig_1=float(freq),
+            _sig_2=float(ampl),
+            _sig_3=float(pw),
+            hival=hival,
+            loval=loval,
+            pw_mode=mode
+        )
 
     def trigger(self):
         # number inputs to the DSP ins (freq, amp) are
--- mfpdsp/builtin_pulse.c
+++ mfpdsp/builtin_pulse.c
@@ -6,19 +6,23 @@
 #include "mfp_dsp.h"
 #include "mfp_block.h"
 
+
+#define PW_MODE_FRAC 0
+#define PW_MODE_MS 1
+
 typedef struct {
     mfp_block * int_phase;
     mfp_block * int_phase_thresh;
     double const_freq;
     double const_ampl;
-    double const_pw_frac;
+    double const_pw;
     double loval;
     double hival;
-    double pw_ms;
+    int pw_mode;
     double phase;
 } builtin_pulse_data;
 
-/* 
+/*
  * square/pulse signal generator
  * signal inputs:
  *    Frequency
@@ -27,18 +31,18 @@ typedef struct {
  * constant params:
  *    lowval: Low value ("off")
  *    hival: High value ("on")
- *    pw_ms: Fixed pulse with (milliseconds)
  *    phase: Resettable phase
  */
 
-static int 
-process_pulse(mfp_processor * proc) 
+static int
+process_pulse(mfp_processor * proc)
 {
     builtin_pulse_data * d = (builtin_pulse_data *)(proc->data);
     int mode_fm = 0;
     int mode_am = 0;
     int mode_pwm = 0;
-    double phase_base;
+
+    double phase_base, phase_scale, freq_base;
     float newphase = 0.0;
     int c;
 
@@ -54,15 +58,21 @@ process_pulse(mfp_processor * proc)
         mode_pwm = 1;
     }
 
-    phase_base = 2.0*M_PI / (double)proc->context->samplerate;
-
     if (proc->outlet_buf[0] == NULL) {
         mfp_proc_error(proc, "No output buffers allocated");
         return 0;
     }
 
+    /* in this block, we want to fill int_phase with the signal
+     * phase (either 0--> 2PI or milliseconds) and fill
+     * int_phase_thresh with the phase threshold for mode flip
+     * (a constant or the scaled PWM signal)
+     */
+    phase_base = 2.0*M_PI / (double)proc->context->samplerate;
+
     if(mode_fm == 1) {
-        newphase = mfp_block_prefix_sum(proc->inlet_buf[0], phase_base, d->phase, d->int_phase); 
+        newphase = mfp_block_prefix_sum(
+            proc->inlet_buf[0], phase_base, d->phase, d->int_phase);
         newphase = fmod(newphase, 2.0*M_PI);
 
         /* wrap the phase to function domain */
@@ -70,19 +80,38 @@ process_pulse(mfp_processor * proc)
 
     }
     else {
-        newphase = mfp_block_phase(d->int_phase, d->phase, phase_base*d->const_freq, 2.0*M_PI);
+        newphase = mfp_block_phase(d->int_phase, d->phase,
+                phase_base*d->const_freq, 2.0*M_PI);
+    }
+
+    /* phase is scaled 0->2PI.  If we are doing fixed pulse,
+     * scale the phase appropriately */
+    if (d->pw_mode == PW_MODE_MS) {
+        phase_scale = 1.0;
+        if (mode_fm) {
+            freq_base = proc->inlet_buf[0]->data[0];
+        }
+        else {
+            freq_base = d->const_freq;
+        }
+        mfp_block_const_mul(d->int_phase,
+            (1000.0 / freq_base) / (2.0*M_PI),
+            d->int_phase);
+    }
+    else {
+        phase_scale = 2.0*M_PI;
     }
 
     if (mode_pwm == 1) {
-        mfp_block_const_mul(proc->inlet_buf[2], 2.0*M_PI, d->int_phase_thresh);
+        mfp_block_const_mul(proc->inlet_buf[2], phase_scale, d->int_phase_thresh);
     }
     else {
-        mfp_block_fill(d->int_phase_thresh, d->const_pw_frac * 2.0 * M_PI);
+        mfp_block_fill(d->int_phase_thresh, d->const_pw * phase_scale);
     }
 
     /* now the real work */
     mfp_block_cmp(d->int_phase, d->int_phase_thresh,
-            d->hival, d->loval, proc->outlet_buf[0]);
+        d->loval, d->hival, proc->outlet_buf[0]);
 
     /* apply gain or amplitude modulation */
     if(mode_am == 1) {
@@ -97,18 +126,18 @@ process_pulse(mfp_processor * proc)
     return 0;
 }
 
-static void 
-init(mfp_processor * proc) 
+static void
+init(mfp_processor * proc)
 {
     builtin_pulse_data * d = g_malloc0(sizeof(builtin_pulse_data));
 
     d->const_freq = 0.0;
     d->const_ampl = 1.0;
-    d->const_pw_frac = 0.5;
+    d->const_pw = 0.5;
     d->phase = 0;
-    d->pw_ms = -1.0;
     d->loval = -1.0;
     d->hival = 1.0;
+    d->pw_mode = PW_MODE_FRAC;
     d->int_phase = mfp_block_new(mfp_max_blocksize);
     d->int_phase_thresh = mfp_block_new(mfp_max_blocksize);
 
@@ -120,7 +149,7 @@ init(mfp_processor * proc)
 }
 
 static void
-destroy(mfp_processor * proc) 
+destroy(mfp_processor * proc)
 {
     builtin_pulse_data * d = (builtin_pulse_data *)(proc->data);
 
@@ -130,48 +159,49 @@ destroy(mfp_processor * proc)
     }
 }
 
-static int 
-config(mfp_processor * proc) 
+static int
+config(mfp_processor * proc)
 {
     builtin_pulse_data * d = (builtin_pulse_data *)(proc->data);
     gpointer freq_ptr = g_hash_table_lookup(proc->params, "_sig_1");
     gpointer ampl_ptr = g_hash_table_lookup(proc->params, "_sig_2");
-    gpointer pw_frac_ptr = g_hash_table_lookup(proc->params, "_sig_3");
+    gpointer pw_ptr = g_hash_table_lookup(proc->params, "_sig_3");
     gpointer phase_ptr = g_hash_table_lookup(proc->params, "phase");
     gpointer loval_ptr = g_hash_table_lookup(proc->params, "loval");
     gpointer hival_ptr = g_hash_table_lookup(proc->params, "hival");
-    gpointer pw_ms_ptr = g_hash_table_lookup(proc->params, "pw_ms");
+    gpointer pw_mode_ptr = g_hash_table_lookup(proc->params, "pw_mode");
 
-    /* if blocksize has changed, resize internal buffer */ 
+    /* if blocksize has changed, resize internal buffer */
     if (d->int_phase->blocksize != proc->context->blocksize) {
         mfp_block_resize(d->int_phase, proc->context->blocksize);
     }
 
-    /* get parameters */ 
+    /* get parameters */
     if (freq_ptr != NULL) {
         d->const_freq = *(float *)freq_ptr;
     }
 
-    if (pw_frac_ptr != NULL) {
-        d->const_pw_frac = *(float *)pw_frac_ptr;
+    if (pw_ptr != NULL) {
+        d->const_pw = *(float *)pw_ptr;
     }
 
     if (loval_ptr != NULL) {
         d->loval = *(float *)loval_ptr;
     }
-    
+
     if (hival_ptr != NULL) {
         d->hival = *(float *)hival_ptr;
     }
 
-    if (pw_ms_ptr != NULL) {
-        d->pw_ms = *(float *)pw_ms_ptr;
+    if (pw_mode_ptr != NULL) {
+        d->pw_mode = (int)(*(float *)pw_mode_ptr);
+        printf("SET PW_MODE TO %d", d->pw_mode);
     }
 
     if (phase_ptr != NULL) {
         d->phase = *(float *)phase_ptr;
         g_hash_table_remove(proc->params, "phase");
-        /* FIXME free in config() */ 
+        /* FIXME free in config() */
         g_free(phase_ptr);
     }
 
@@ -180,7 +210,7 @@ config(mfp_processor * proc)
 
 
 
-mfp_procinfo *  
+mfp_procinfo *
 init_builtin_pulse(void) {
     mfp_procinfo * p = g_malloc0(sizeof(mfp_procinfo));
     p->name = strdup("pulse~");
@@ -195,7 +225,7 @@ init_builtin_pulse(void) {
     g_hash_table_insert(p->params, "_sig_3", (gpointer)PARAMTYPE_FLT);
     g_hash_table_insert(p->params, "loval", (gpointer)PARAMTYPE_FLT);
     g_hash_table_insert(p->params, "hival", (gpointer)PARAMTYPE_FLT);
-    g_hash_table_insert(p->params, "pw_ms", (gpointer)PARAMTYPE_FLT);
+    g_hash_table_insert(p->params, "pw_mode", (gpointer)PARAMTYPE_INT);
     g_hash_table_insert(p->params, "phase", (gpointer)PARAMTYPE_FLT);
 
     return p;
