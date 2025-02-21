bf3de5a3f790affef68d759bd32afe7fafdad82f
Bill Gribble <grib@billgribble.com>
2025-02-21 14:37:41 -0500

    Move Faust compile into a separate thread

--- mfp/builtins/faust.py
+++ mfp/builtins/faust.py
@@ -21,7 +21,7 @@ class Faust(Processor):
     RESP_DSP_OUTLETS = 2
 
     def __init__(self, init_type, init_args, patch, scope, name, defs=None):
-        Processor.__init__(self, 1, 0, init_type, init_args, patch, scope, name)
+        Processor.__init__(self, 1, 0, init_type, init_args, patch, scope, name, defs)
         extra = defs or {}
         initargs, kwargs = self.parse_args(init_args, **extra)
 
@@ -34,7 +34,7 @@ class Faust(Processor):
         self.dsp_outlets = []
 
     async def setup(self):
-        await self.dsp_init( "faust~", faust_code=self.faust_code)
+        await self.dsp_init("faust~", faust_code=self.faust_code)
 
     def dsp_response(self, resp_id, resp_value):
         if resp_id == self.RESP_DSP_INLETS:
@@ -49,9 +49,18 @@ class Faust(Processor):
             inlets,
             self.faust_dsp_outlets
         )
+        need_conf = False
+        if (
+            len(self.dsp_inlets) != self.faust_dsp_inlets 
+            or len(self.dsp_outlets) != self.faust_dsp_outlets 
+        ):
+            need_conf = True
         self.dsp_inlets = list(range(self.faust_dsp_inlets))
         self.dsp_outlets = list(range(self.faust_dsp_outlets))
         self.hot_inlets = list(range(inlets))
+
+        if need_conf:
+            self.conf(dsp_inlets=self.dsp_inlets, dsp_outlets=self.dsp_outlets)
 
     async def trigger(self):
         for inlet_num, value in enumerate(self.inlets):
--- mfpdsp/builtin_faust.c
+++ mfpdsp/builtin_faust.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <glib.h>
+#include <pthread.h>
 
 #include "faust/dsp/llvm-dsp-c.h"
 #include "faust/dsp/libfaust-c.h"
@@ -19,6 +20,15 @@ typedef struct {
     gpointer faust_params;
     int sig_inputs;
     int sig_outputs;
+
+    pthread_t compile_thread;
+    int compile_thread_finished;
+    llvm_dsp_factory * next_faust_factory;
+    llvm_dsp * next_faust_dsp;
+    FAUSTFLOAT ** next_faust_inbufs;
+    FAUSTFLOAT ** next_faust_outbufs;
+    FAUSTFLOAT * next_faust_buffers;
+
 } builtin_faust_data;
 
 typedef struct {
@@ -33,6 +43,28 @@ typedef struct {
 #define RESP_DSP_INLETS 1
 #define RESP_DSP_OUTLETS 2
 
+
+static void
+faust_cleanup_dsp(mfp_processor * proc) {
+    builtin_faust_data * d = (builtin_faust_data *)(proc->data);
+    if (d->faust_dsp) {
+        deleteCDSPInstance(d->faust_dsp);
+        d->faust_dsp = NULL;
+    }
+
+    if (d->faust_factory) {
+        deleteCDSPFactory(d->faust_factory);
+        d->faust_factory = NULL;
+    }
+
+    if (d->faust_buffers) {
+        g_free(d->faust_buffers);
+        d->faust_buffers = NULL;
+    }
+}
+
+
+
 static int
 process(mfp_processor * proc)
 {
@@ -40,6 +72,33 @@ process(mfp_processor * proc)
     mfp_sample * inptr, * outptr;
 
     int blocksize = proc->context->blocksize;
+
+    /* to make sure compile output is synced with the execution loop, */
+    /* update the processor state here */
+    if (d->compile_thread && d->compile_thread_finished) {
+        faust_cleanup_dsp(proc);
+        pthread_join(d->compile_thread, NULL);
+        d->compile_thread = (pthread_t)0;
+        d->compile_thread_finished = 0;
+
+        d->faust_dsp = d->next_faust_dsp;
+        d->faust_factory = d->next_faust_factory;
+        d->faust_inbufs = d->next_faust_inbufs;
+        d->faust_outbufs = d->next_faust_outbufs;
+        d->faust_buffers = d->next_faust_buffers;
+        if (d->faust_dsp) {
+            d->sig_inputs = getNumInputsCDSPInstance(d->faust_dsp);
+            d->sig_outputs = getNumOutputsCDSPInstance(d->faust_dsp);
+        }
+        else {
+            d->sig_inputs = 0;
+            d->sig_outputs = 0;
+        }
+
+        /* reconfigure buffers in the processor object */
+        mfp_proc_free_buffers(proc);
+        mfp_proc_alloc_buffers(proc, d->sig_inputs, d->sig_outputs, proc->context->blocksize);
+    }
 
     if ((proc == NULL) || (d->faust_dsp == NULL)){
         return 0;
@@ -74,7 +133,6 @@ init(mfp_processor * proc)
     builtin_faust_data * d = g_malloc0(sizeof(builtin_faust_data));
     d->faust_code = NULL;
     d->faust_params = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
-
     proc->data = (void *)d;
     return;
 }
@@ -165,13 +223,13 @@ faust_prm_bool(
     prm->prm_min = 0;
     prm->prm_max = 1;
     g_hash_table_insert(d->faust_params, prm->prm_label, prm);
-    g_hash_table_insert(((mfp_processor *)proc)->typeinfo->params, label, (gpointer)PARAMTYPE_FLT);
+    g_hash_table_insert(((mfp_processor *)proc)->typeinfo->params, g_strdup(label), (gpointer)PARAMTYPE_FLT);
     mfp_dsp_send_response_str(proc, RESP_PARAM, g_strdup(label));
 }
 
 
 static void
-faust_config_params(mfp_processor * proc) {
+faust_config_params(mfp_processor * proc, llvm_dsp * dsp) {
     builtin_faust_data * d = (builtin_faust_data *)(proc->data);
 
     UIGlue ui_controls;
@@ -189,7 +247,7 @@ faust_config_params(mfp_processor * proc) {
     ui_controls.addHorizontalBargraph = faust_prm_ignore_number;
     ui_controls.declare = faust_prm_ignore_declare;
 
-    buildUserInterfaceCDSPInstance(d->faust_dsp, &ui_controls);
+    buildUserInterfaceCDSPInstance(dsp, &ui_controls);
 }
 
 
@@ -197,63 +255,87 @@ static int
 faust_config(mfp_processor * proc) {
     builtin_faust_data * d = (builtin_faust_data *)(proc->data);
     char error_msg[4096];
-    if (d->faust_dsp) {
-        deleteCDSPInstance(d->faust_dsp);
-        d->faust_dsp = NULL;
-    }
 
-    if (d->faust_factory) {
-        deleteCDSPFactory(d->faust_factory);
-        d->faust_factory = NULL;
-    }
+    llvm_dsp_factory * faust_factory;
+    llvm_dsp * faust_dsp;
+    FAUSTFLOAT ** faust_inbufs;
+    FAUSTFLOAT ** faust_outbufs;
+    FAUSTFLOAT * faust_buffers;
 
-    if (d->faust_buffers) {
-        g_free(d->faust_buffers);
-        d->faust_buffers = NULL;
-    }
+    if (d->faust_code && strlen(d->faust_code) >= 2) {
+        faust_factory = createCDSPFactoryFromString(
+            "mfp_faust", d->faust_code, 0, NULL, "", error_msg, -1
+        );
 
-    if (!d->faust_code || strlen(d->faust_code) < 2) {
-        return 0;
-    }
-
-    d->faust_factory = createCDSPFactoryFromString(
-        "mfp_faust", d->faust_code, 0, NULL, "", error_msg, -1
-    );
-
-    if (!d->faust_factory) {
-        mfp_log_debug("[faust~] Cannot create JIT factory : %s\n", error_msg);
-    }
-    else {
-        d->faust_dsp = createCDSPInstance(d->faust_factory);
-        if (!d->faust_dsp) {
-            mfp_log_debug("[faust~] Cannot create DSP engine\n");
-        } else {
-            d->sig_inputs = getNumInputsCDSPInstance(d->faust_dsp);
-            d->sig_outputs = getNumOutputsCDSPInstance(d->faust_dsp);
-
-            /* reconfigure buffers in the processor object */
-            mfp_proc_free_buffers(proc);
-            mfp_proc_alloc_buffers(proc, d->sig_inputs, d->sig_outputs, proc->context->blocksize);
-            d->faust_buffers = g_malloc0(
-                (d->sig_inputs + d->sig_outputs)*sizeof(FAUSTFLOAT)*proc->context->blocksize
-            );
-            d->faust_inbufs = g_malloc0((d->sig_inputs + 1) * sizeof(FAUSTFLOAT *));
-            d->faust_outbufs = g_malloc0((d->sig_outputs + 1) * sizeof(FAUSTFLOAT *));
-
-            for (int in = 0; in < d->sig_inputs; in++) {
-                d->faust_inbufs[in] = d->faust_buffers + in * proc->context->blocksize;
-            }
-            FAUSTFLOAT * out_start = d->faust_buffers + d->sig_inputs * proc->context->blocksize;
-            for (int out = 0; out < d->sig_outputs; out++) {
-                d->faust_outbufs[out] = out_start + out * proc->context->blocksize;
-            }
-
-            /* init the DSP instance */
-            initCDSPInstance(d->faust_dsp, proc->context->samplerate);
-
-            /* extract parameters */
-            faust_config_params(proc);
+        if (!faust_factory) {
+            mfp_log_debug("[faust~] Cannot create JIT factory : %s\n", error_msg);
+            return -1;
         }
+        else {
+            faust_dsp = createCDSPInstance(faust_factory);
+            if (!faust_dsp) {
+                mfp_log_debug("[faust~] Cannot create DSP engine\n");
+            } else {
+                int sig_inputs = getNumInputsCDSPInstance(faust_dsp);
+                int sig_outputs = getNumOutputsCDSPInstance(faust_dsp);
+                faust_buffers = g_malloc0(
+                    (sig_inputs + sig_outputs)*sizeof(FAUSTFLOAT)*proc->context->blocksize
+                );
+                faust_inbufs = g_malloc0((sig_inputs + 1) * sizeof(FAUSTFLOAT *));
+                faust_outbufs = g_malloc0((sig_outputs + 1) * sizeof(FAUSTFLOAT *));
+
+                for (int in = 0; in < sig_inputs; in++) {
+                    faust_inbufs[in] = faust_buffers + in * proc->context->blocksize;
+                }
+                FAUSTFLOAT * out_start = faust_buffers + sig_inputs * proc->context->blocksize;
+                for (int out = 0; out < sig_outputs; out++) {
+                    faust_outbufs[out] = out_start + out * proc->context->blocksize;
+                }
+
+                /* init the DSP instance */
+                initCDSPInstance(faust_dsp, proc->context->samplerate);
+
+                /* extract parameters */
+                faust_config_params(proc, faust_dsp);
+
+                mfp_dsp_send_response_int(proc, RESP_DSP_INLETS, sig_inputs);
+                mfp_dsp_send_response_int(proc, RESP_DSP_OUTLETS, sig_outputs);
+            }
+        }
+        d->next_faust_factory = faust_factory;
+        d->next_faust_dsp = faust_dsp;
+        d->next_faust_inbufs = faust_inbufs;
+        d->next_faust_outbufs = faust_outbufs;
+        d->next_faust_buffers = faust_buffers;
+    }
+    return 0;
+}
+
+
+static void *
+faust_recompile_thread(void * data) {
+    mfp_processor * proc = (mfp_processor *)data;
+    builtin_faust_data * d = (builtin_faust_data *)(proc->data);
+    faust_config(proc);
+    d->compile_thread_finished = 1;
+    pthread_exit(NULL);
+    return NULL;
+}
+
+
+static void
+start_recompile_thread(mfp_processor * proc) {
+    builtin_faust_data * d = (builtin_faust_data *)(proc->data);
+    d->compile_thread_finished = 0;
+    int create_error = pthread_create(
+        &(d->compile_thread),
+        NULL,
+        faust_recompile_thread,
+        (void *)proc
+    );
+    if (create_error != 0) {
+        d->compile_thread = (pthread_t)0;
+        mfp_log_error("[faust~] Could not create Faust compile thread\n");
     }
 }
 
@@ -290,9 +372,8 @@ config(mfp_processor * proc)
         if (code) {
             d->faust_code = g_strdup((char *)code);
         }
-        faust_config(proc);
-        mfp_dsp_send_response_int(proc, RESP_DSP_INLETS, d->sig_inputs);
-        mfp_dsp_send_response_int(proc, RESP_DSP_OUTLETS, d->sig_outputs);
+        start_recompile_thread(proc);
+
     }
     else {
         g_hash_table_foreach(
@@ -310,7 +391,7 @@ mfp_procinfo *
 init_builtin_faust(void) {
     mfp_procinfo * p = g_malloc0(sizeof(mfp_procinfo));
     p->name = strdup("faust~");
-    p->is_generator = 0;
+    p->is_generator = GENERATOR_CONDITIONAL;
     p->process = process;
     p->init = init;
     p->destroy = destroy;
